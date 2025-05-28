f5f659933baae7db93cc382d46b78ab30083092d
Veng715 <ramzirichani2005@gmail.com>
2025-05-28 18:22:06 -0400

    Add files via upload

---
+++ Módulo 1/Tareas de Python 2/Tareas de python 1/# Número par o impar.py
@@ -0,0 +1,6 @@
+# Número par o impar
+numero = int(input("coloca un número entero: "))
+if numero % 2 == 0:
+    print(numero, "es un número par.")
+else:
+    print(numero, "es un número impar.")
---
+++ Módulo 1/Tareas de Python 2/Tareas de python 1/# Verificación de edad para votar.py
@@ -0,0 +1,6 @@
+# Verificación de edad para votar
+edad = int(input("¿cuántos años tienes?: "))
+if edad < 18:
+    print("no puedes votar.")
+else:
+    print("puedes votar.")

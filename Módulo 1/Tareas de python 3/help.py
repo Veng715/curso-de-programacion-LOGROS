842413d1101a5de069a70899aedf3a526f1caad4
Veng715 <ramzirichani2005@gmail.com>
2025-05-29 13:40:14 -0400

    Update help.py

--- Módulo 1/Tareas de python 3/help.py
+++ Módulo 1/Tareas de python 3/help.py
@@ -1,12 +1,12 @@
-def():
-print("Este es su carrito de compras\n")
-print("Elija una de las siguientes opciones:")
-print("1. Añadir")
-print("2. Ver")
-print("3. Remover")
-print("4. Suma total")
-print("5. Salir")
-opciones = input("¿Qué elección deseas elegir?: ")
+articulos = ["bolsa de pan", "refresco", "café", "arroz", "galletas"]
+precios = ["$5", "$5", "$3", "$2", "$3"]
 
-articulos = []
-precios = []
+print("Este es su carrito de compras")
+print("Elija una de las siguientes opciones:")
+print("1. Añadir.")
+print("2. Ver.")
+print("3. Remover.")
+print("4. Suma total.")
+print("5. Salir.")
+
+opciones = input("¿Qué elección deseas elegir?: ")

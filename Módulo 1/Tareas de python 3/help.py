9df9fc47780010097f04ddd3107600227b69d9dd
Veng715 <ramzirichani2005@gmail.com>
2025-05-29 13:24:56 -0400

    Add files via upload

---
+++ Módulo 1/Tareas de python 3/help.py
@@ -0,0 +1,12 @@
+def():
+print("Este es su carrito de compras\n")
+print("Elija una de las siguientes opciones:")
+print("1. Añadir")
+print("2. Ver")
+print("3. Remover")
+print("4. Suma total")
+print("5. Salir")
+opciones = input("¿Qué elección deseas elegir?: ")
+
+articulos = []
+precios = []
